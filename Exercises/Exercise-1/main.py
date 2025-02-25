f02a20debd543e453fff218df07a944a6cefe544
amirhamzahbmn <amirhamzahbmn@gmail.com>
2025-02-25 19:07:08 +0800

    completed exercise, will revisit to attempt async method near future

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,4 +1,7 @@
 import requests
+import os
+from pathlib import Path
+from zipfile import ZipFile
 
 download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
@@ -10,11 +13,29 @@ download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2220_Q1.zip",
 ]
 
-
 def main():
-    # your code here
-    pass
+    for link in download_uris:
+        file_name = link.split('/')[3].split('.')[0]
 
+        print(f'Attempting to download {file_name}..')
+        response = requests.get(link)
+
+        if response.status_code != 200:
+            print(f'{link} is an invalid URI')
+            continue
+
+
+        output_file = Path(f'downloads/{file_name}.zip')
+        output_file.parent.mkdir(exist_ok=True, parents=True)
+        with open(output_file, 'wb') as f:
+            f.write(response.content)
+
+        with ZipFile(output_file) as zipref:
+            zipref.extract(f'{file_name}.csv', path='./downloads')
+
+        os.remove(output_file)
+
+        print(f'Downloaded {file_name}!')
 
 if __name__ == "__main__":
     main()
