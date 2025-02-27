31f2b8e283f001044fdcfb567ec2c995d8262b05
amirhamzahbmn <amirhamzahbmn@gmail.com>
2025-02-27 18:33:22 +0800

    completed q5

--- Exercises/Exercise-5/main.py
+++ Exercises/Exercise-5/main.py
@@ -1,5 +1,5 @@
 import psycopg2
-
+import csv
 
 def main():
     host = "postgres"
@@ -9,7 +9,55 @@ def main():
     conn = psycopg2.connect(host=host, database=database, user=user, password=pas)
     # your code here
     cur = conn.cursor()
+    cur.execute("""
+                CREATE TABLE products(
+                    product_id INT
+                    ,product_code TEXT
+                    ,product_description TEXT
+                    ,PRIMARY KEY(product_id)
+                );
 
+                CREATE TABLE accounts(
+                    customer_id INT
+                    ,first_name TEXT
+                    ,last_name TEXT
+                    ,address_1 TEXT
+                    ,address_2 TEXT
+                    ,city TEXT
+                    ,state TEXT
+                    ,zip_code TEXT
+                    ,join_date DATE
+                    ,PRIMARY KEY(customer_id)
+                );
+
+                CREATE TABLE transactions(
+                    transaction_id TEXT
+                    ,transaction_date DATE
+                    ,product_id INT
+                    ,product_code TEXT
+                    ,product_description TEXT
+                    ,quantity INT
+                    ,account_id INT
+                    ,PRIMARY KEY(transaction_id)
+                    ,CONSTRAINT fk_account_id
+                        FOREIGN KEY(account_id)
+                        REFERENCES accounts(customer_id)
+                    ,CONSTRAINT fk_product_id
+                        FOREIGN KEY(product_id)
+                        REFERENCES products(product_id)
+                );
+                """)
+    conn.commit()
+    with open('data/accounts.csv', 'r') as f:
+        next(f)
+        cur.copy_from(f, 'accounts', sep=',')
+    with open('data/products.csv', 'r') as f:
+        next(f)
+        cur.copy_from(f, 'products', sep=',')
+    with open('data/transactions.csv', 'r') as f:
+        next(f)
+        cur.copy_from(f, 'transactions', sep=',')
+    conn.commit()
 
 if __name__ == "__main__":
     main()
